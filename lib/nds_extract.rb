2c2875106d057721e50d25be51585bc8b6091c95
James Rogers <jarogers095@gmail.com>
2019-12-01 23:46:27 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,5 +1,6 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require_relative './directors_database'
+require 'pp'
 
 def directors_totals(source)
   result = {}
@@ -25,10 +26,11 @@ def gross_for_director(d)
 end
 
 def list_of_directors(source)
-  directors = []
-  source.each do |n|
-    directors.push(n[:name])
-  end
+  pp source
+  #directors = []
+  #source.each do |n|
+  #  directors.push(n[:name])
+  #end
 end
 
 def total_gross(source)
